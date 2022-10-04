b17ea4bc48fd04cff90c9d9e80631c91027866da
bexalho <aljesa.hoxha@bexeo.com>
2022-10-04 13:36:14 +0200

    dynamic styles with css modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,32 +1,32 @@
 import { useState } from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-    background: ${(props) => (props.invalid ? "#d3002d17" : "transparent")};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #004678;
-  }
-`;
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+//     background: ${(props) => (props.invalid ? "#d3002d17" : "transparent")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #004678;
+//   }
+// `;
 
 function CourseInput(props) {
   const [enteredValue, setEnteredValue] = useState("");
@@ -46,7 +46,9 @@ function CourseInput(props) {
   };
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input
           type="text"
@@ -61,7 +63,7 @@ function CourseInput(props) {
             The input should not be empty
           </p>
         )}
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
