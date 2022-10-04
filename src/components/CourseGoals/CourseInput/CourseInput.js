ca509cb61c0080c6d21fd087453698d8b4379e1d
bexalho <aljesa.hoxha@bexeo.com>
2022-10-04 11:26:24 +0200

    added FormControl styled component

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,6 +1,36 @@
 import { useState } from "react";
+import styled from "styled-components";
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #004678;
+  }
+
+  &.invalid input {
+    border-color: red;
+    background-color: #d3002d17;
+  }
+`;
 
 function CourseInput(props) {
   const [enteredValue, setEnteredValue] = useState("");
@@ -20,7 +50,7 @@ function CourseInput(props) {
   };
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+      <FormControl className={!isValid && "invalid"}>
         <label>Course Goal</label>
         <input
           type="text"
@@ -35,7 +65,7 @@ function CourseInput(props) {
             The input should not be empty
           </p>
         )}
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
