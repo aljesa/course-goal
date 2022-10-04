571aa62f274f0958a10139a771996537fd11fec7
bexalho <aljesa.hoxha@bexeo.com>
2022-10-04 11:19:27 +0200

    added styled-components package

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,32 @@
-import "./Button.css";
+// import "./Button.css";
+import styled from "styled-components";
 
-function Button(props) {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-}
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #004678;
+  color: white;
+  background: #004678;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #004678;
+    border-color: #004678;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
+
+// function Button(props) {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// }
 export default Button;
